a788f41a171eb7e44835e86a78c8c0af34bca08e
Chanaka Abeysinghe <chanakasan@gmail.com>
2025-03-09 14:02:53 +0530

    refac

--- stash/all.py
+++
@@ -1,8 +0,0 @@
-
-def create_new_notebook(name):
-  old_name = 'new space ##.sublime-workspace'
-  new_name = name + '.sublime-workspace'
-  src_path = notes_base_path() + "/root/" + old_name
-  new_path = notes_base_path() + "/notepads/" + new_name
-  copy_file_safely(src_path, new_path)
-  return new_path
